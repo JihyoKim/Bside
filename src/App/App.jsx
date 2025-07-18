f5a92f4da52ce230e34dfca4dd404029aeeeb341
areumeom <9914arar@gmail.com>
2025-07-18 11:24:04 +0900

    App.jsx 온보딩 경로 중복 제거 및 라우팅 정리

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-// ✅ 온보딩 페이지
+// 온보딩
 import Step1 from '../pages/onboarding/Step1_Account';
 import Step2 from '../pages/onboarding/Step2_Pin';
 import Step3 from '../pages/onboarding/Step3_Password';
@@ -10,6 +10,7 @@ import Step5 from '../pages/onboarding/Step5_Language';
 import Step6 from '../pages/onboarding/Step6_AddArtists';
 import Step7 from '../pages/onboarding/Step7_Complete';
 
+// 메인
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Music from '../pages/Music';
@@ -19,8 +20,8 @@ import ADPShop from '../pages/shopPage/ADPShop';
 import RiizeShop from '../pages/shopPage/RiizeShop';
 import BPShop from '../pages/shopPage/BPShop';
 import MyPage from '../pages/MyPage';
-import ArtistPage from '../pages/ArtistPage'; 
-import Media from '../pages/artistPage/Media'; 
+import ArtistPage from '../pages/ArtistPage';
+import Media from '../pages/artistPage/Media';
 import Notice from '../pages/artistPage/Notice';
 import Vote from '../pages/artistPage/Vote';
 import Fan from '../pages/artistPage/Fan';
@@ -29,10 +30,10 @@ import MoreArtist from '../pages/MoreArtist';
 const App = () => {
   return (
     <Routes>
-      {/* ✅ 기본 경로 진입 시 온보딩으로 리디렉션 */}
+      {/* 기본 진입 시 온보딩으로 이동 */}
       <Route path="/" element={<Navigate to="/onboarding" replace />} />
 
-      {/* ✅ 온보딩 경로 분리 */}
+      {/* 온보딩 */}
       <Route path="/onboarding">
         <Route index element={<Step1 />} />
         <Route path="pin" element={<Step2 />} />
@@ -43,7 +44,7 @@ const App = () => {
         <Route path="complete" element={<Step7 />} />
       </Route>
 
-      {/* ✅ 메인 앱 라우트 */}
+      {/* 메인 앱 라우트 */}
       <Route path="/main" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path='music' element={<Music />} />
@@ -62,17 +63,11 @@ const App = () => {
           <Route path='notice' element={<Notice />} />
           <Route path='vote' element={<Vote />} />
           <Route path='fan' element={<Fan />} />
-        </Route>        
-          <Route path='artistPage/:artistId' element={<ArtistPage />} >
-          <Route index element={<Media />} />
-          <Route path='media' element={<Media />} />
-          <Route path='notice' element={<Notice />} />
-          <Route path='vote' element={<Vote />} />
-          <Route path='fan' element={<Fan />} />
         </Route>
-        {/* 잘못된 경로는 메인 홈으로 이동 */}
-        <Route path="*" element={<Navigate to="/main" replace />} />
       </Route>
+
+      {/* 잘못된 경로는 온보딩으로 */}
+      <Route path="*" element={<Navigate to="/onboarding" replace />} />
     </Routes>
   );
 };
