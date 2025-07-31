3508610aae89ac4a9e3195f334468ade5a392a98
JihyoKim <jihyoxo@gmail.com>
2025-07-31 18:18:49 +0900

    테스트

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -87,17 +87,7 @@ const Layout = () => {
     document.documentElement.classList.add('mobile-device');
   }
 
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
   
-    setVh(); // 초기 설정
-    window.addEventListener('resize', setVh);
-  
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
 
 
   return (
